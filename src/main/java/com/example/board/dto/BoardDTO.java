55e5d90bc3d4717f6b770ea5c6f90f438afd8823
jina ham <hamjina61@gmail.com>
2025-01-06 14:37:25 +0900

    feat: 게시글 작성

---
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -0,0 +1,23 @@
+package com.example.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private LocalDateTime boardCreatedTime;
+    private LocalDateTime boardUpdatedTime;
+}
---
+++ src/main/java/com/example/board/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+//시간정보를 다루기 위한 클래스
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime updatedTime;
+}
